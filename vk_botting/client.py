5c0a7f3093d101a338f09a410bab33fa4d6fc1f6
MrDandycorn <ypsen@yandex.ru>
2019-12-15 00:41:54 +0300

    Разделил UserClient и Client (задел на будущее)

--- vk_botting/client.py
+++ vk_botting/client.py
@@ -77,7 +77,6 @@ class Client:
         self.v = kwargs.get('v', '5.999')
         self.force = kwargs.get('force', False)
         self.loop = asyncio.get_event_loop()
-        self.user = None
         self.group = None
         self.key = None
         self.server = None
@@ -143,24 +142,6 @@ class Client:
         events = dict([(event, 1) for event in self._implemented_events])
         res = await self.vk_request('groups.setLongPollSettings', group_id=self.group.id, enabled=1, api_version='5.103', **events)
         return res
-
-    async def get_user_longpoll(self):
-        res = await self.vk_request('messages.getLongPollServer', group_id=self.group.id, lp_version=3)
-        error = res.get('error', None)
-        if error and error['error_code'] == 15:
-            raise LoginError('User has no access to messages API. Try generating token with vk_botting.auth methods')
-        elif error and error['error_code'] == 100:
-            if self.force:
-                await self.enable_longpoll()
-                return await self.get_longpoll_server()
-            raise VKApiError('Longpoll is disabled for this group. Enable longpoll or try force mode')
-        elif error:
-            raise VKApiError(f'[{error["error_code"]}]{error["error_msg"]}')
-        self.key = res['response']['key']
-        server = res['response']['server'].replace(r'\/', '/')
-        self.server = f'https://{server}'
-        ts = res['response']['ts']
-        return ts
 
     async def get_longpoll_server(self):
         res = await self.vk_request('groups.getLongPollServer', group_id=self.group.id)
@@ -303,6 +284,222 @@ class Client:
         elif t not in self._implemented_events and 'on_unknown' in self.extra_events:
             return self.dispatch('unknown', update)
 
+    def dispatch(self, event, *args, **kwargs):
+        method = 'on_' + event
+        listeners = self._listeners.get(event)
+        if listeners:
+            removed = []
+            for i, (future, condition) in enumerate(listeners):
+                if future.cancelled():
+                    removed.append(i)
+                    continue
+
+                try:
+                    result = condition(*args)
+                except Exception as exc:
+                    future.set_exception(exc)
+                    removed.append(i)
+                else:
+                    if result:
+                        if len(args) == 0:
+                            future.set_result(None)
+                        elif len(args) == 1:
+                            future.set_result(args[0])
+                        else:
+                            future.set_result(args)
+                        removed.append(i)
+
+            if len(removed) == len(listeners):
+                self._listeners.pop(event)
+            else:
+                for idx in reversed(removed):
+                    del listeners[idx]
+
+        try:
+            coro = getattr(self, method)
+        except AttributeError:
+            pass
+        else:
+            self._schedule_event(coro, method, *args, **kwargs)
+
+    async def on_error(self, event_method, *args, **kwargs):
+        print('Ignoring exception in {}'.format(event_method), file=sys.stderr)
+        traceback.print_exc()
+
+    async def _run_event(self, coro, event_name, *args, **kwargs):
+        try:
+            await coro(*args, **kwargs)
+        except asyncio.CancelledError:
+            pass
+        except Exception:
+            try:
+                await self.on_error(event_name, *args, **kwargs)
+            except asyncio.CancelledError:
+                pass
+
+    def _schedule_event(self, coro, event_name, *args, **kwargs):
+        wrapped = self._run_event(coro, event_name, *args, **kwargs)
+        return _ClientEventTask(original_coro=coro, event_name=event_name, coro=wrapped, loop=self.loop)
+
+    async def get_user(self, uid):
+        user = await get_users(self.token, uid)
+        if user:
+            return user[0]
+        return None
+
+    async def get_group(self, gid):
+        group = await get_groups(self.token, gid)
+        if group:
+            return group[0]
+        return None
+
+    async def send_message(self, peer_id=None, message=None, *, attachment=None, sticker_id=None, keyboard=None, reply_to=None, forward_messages=None):
+        if attachment:
+            attachment = ','.join(map(str, attachment))
+        params = {'group_id': self.group.id, 'random_id': randint(-2 ** 63, 2 ** 63 - 1), 'peer_id': peer_id, 'message': message, 'attachment': attachment,
+                  'reply_to': reply_to, 'forward_messages': forward_messages, 'sticker_id': sticker_id, 'keyboard': keyboard}
+        res = await self.vk_request('messages.send', **params)
+        if 'error' in res.keys():
+            raise VKApiError('[{error_code}] {error_msg}'.format(**res['error']))
+        params['id'] = res['response']
+        params['from_id'] = -self.group.id
+        return await build_msg(params, self)
+
+    async def _run(self, owner_id):
+        if owner_id and owner_id.__class__ is not int:
+            raise TypeError(f'Owner_id must be positive integer, not {owner_id.__class__.__name__}')
+        if owner_id and owner_id < 0:
+            raise VKApiError(f'Owner_id must be positive integer')
+        user = await get_own_page(self.token)
+        if isinstance(user, Group):
+            self.is_group = True
+            self.group = user
+            if self.is_group and owner_id:
+                raise VKApiError('Owner_id passed together with group access_token')
+            ts = await self.get_longpoll_server()
+            self.dispatch('ready')
+            updates = []
+            while True:
+                try:
+                    lp = self.loop.create_task(self.longpoll(ts))
+                    for update in updates:
+                        self.loop.create_task(self.handle_update(update))
+                    ts, updates = await lp
+                except Exception as e:
+                    print(f'Ignoring exception in longpoll cycle:\n{e}', file=sys.stderr)
+                    ts = await self.get_longpoll_server()
+        raise LoginError('User token passed to group client')
+
+    def run(self, token, owner_id=None):
+        self.token = token
+        self.loop.create_task(self._run(owner_id))
+        self.loop.run_forever()
+
+
+class UserClient:
+
+    def __init__(self, **kwargs):
+        self.v = kwargs.get('v', '5.999')
+        self.user_agent = kwargs.get('user_agent', 'KateMobileAndroid/52.1 lite-445 (Android 4.4.2; SDK 19; x86; unknown Android SDK built for x86; en)')
+        self.force = kwargs.get('force', False)
+        self.loop = asyncio.get_event_loop()
+        self.user = None
+        self.key = None
+        self.server = None
+        self.old_longpoll = kwargs.get('old_longpoll', False)
+        self._listeners = {}
+        timeout = aiohttp.ClientTimeout(total=100, connect=10)
+        self.session = aiohttp.ClientSession(timeout=timeout)
+        self._implemented_events = ['message_new']
+
+    def Payload(self, **kwargs):
+        kwargs['access_token'] = self.token
+        kwargs['v'] = self.v
+        return kwargs
+
+    class botCommandException(Exception):
+        pass
+
+    def wait_for(self, event, *, check=None, timeout=None):
+        future = self.loop.create_future()
+        if check is None:
+            def _check(*args):
+                return True
+            check = _check
+
+        ev = event.lower()
+        try:
+            listeners = self._listeners[ev]
+        except KeyError:
+            listeners = []
+            self._listeners[ev] = listeners
+
+        listeners.append((future, check))
+        return asyncio.wait_for(future, timeout, loop=self.loop)
+
+    async def general_request(self, url, post=False, **params):
+        for param in list(params):
+            if params[param] is None:
+                params.pop(param)
+            elif not isinstance(params[param], (str, int)):
+                params[param] = str(params[param])
+            elif isinstance(params[param], bool):
+                params[param] = str(params[param])
+        for tries in range(5):
+            try:
+                req = self.session.post(url, data=params) if post else self.session.get(url, params=params)
+                async with req as r:
+                    if r.content_type == 'application/json':
+                        return await r.json()
+                    return await r.text()
+            except Exception as e:
+                print(f'Got exception in request: {e}\nRetrying in {tries*2+1} seconds', file=sys.stderr)
+                await asyncio.sleep(tries*2+1)
+
+    async def vk_request(self, method, **kwargs):
+        res = await self.general_request(f'https://api.vk.com/method/{method}', **self.Payload(**kwargs))
+        error = res.get('error', None)
+        if error and error['error_code'] == 6:
+            await asyncio.sleep(1)
+            return await self.vk_request(method, **kwargs)
+        return res
+
+    async def enable_longpoll(self):
+        events = dict([(event, 1) for event in self._implemented_events])
+        res = await self.vk_request('groups.setLongPollSettings', group_id=self.group.id, enabled=1, api_version='5.103', **events)
+        return res
+
+    async def get_user_longpoll(self):
+        res = await self.vk_request('messages.getLongPollServer', group_id=self.group.id, lp_version=3)
+        error = res.get('error', None)
+        if error and error['error_code'] == 15:
+            raise LoginError('User has no access to messages API. Try generating token with vk_botting.auth methods')
+        elif error:
+            raise VKApiError(f'[{error["error_code"]}]{error["error_msg"]}')
+        self.key = res['response']['key']
+        server = res['response']['server'].replace(r'\/', '/')
+        self.server = f'https://{server}'
+        ts = res['response']['ts']
+        return ts
+
+    async def longpoll(self, ts):
+        payload = {'key': self.key,
+                   'act': 'a_check',
+                   'ts': ts,
+                   'wait': '10'}
+        if not self.is_group:
+            payload['mode'] = 10
+        try:
+            res = await self.general_request(self.server, **payload)
+        except asyncio.TimeoutError:
+            return ts, []
+        if 'ts' not in res.keys() or 'failed' in res.keys():
+            ts = await self.get_user_longpoll()
+        else:
+            ts = res['ts']
+        updates = res.get('updates', [])
+        return ts, updates
+
     async def handle_user_update(self, update):
         t = update.pop(0)
         if t == 4 and 'on_message_new' in self.extra_events:
@@ -406,7 +603,7 @@ class Client:
         if owner_id and owner_id < 0:
             raise VKApiError(f'Owner_id must be positive integer')
         user = await get_own_page(self.token)
-        if user.__class__ is User:
+        if isinstance(user, User):
             self.is_group = False
             self.group = Group({})
             self.user = user
@@ -421,25 +618,8 @@ class Client:
                     ts, updates = await lp
                 except Exception as e:
                     print(f'Ignoring exception in longpoll cycle:\n{e}', file=sys.stderr)
-                    ts = await self.get_longpoll_server()
-        else:
-            self.is_group = True
-            self.group = user
-            self.user = User({})
-            if self.is_group and owner_id:
-                raise VKApiError('Owner_id passed together with group access_token')
-            ts = await self.get_longpoll_server()
-            self.dispatch('ready')
-            updates = []
-            while True:
-                try:
-                    lp = self.loop.create_task(self.longpoll(ts))
-                    for update in updates:
-                        self.loop.create_task(self.handle_update(update))
-                    ts, updates = await lp
-                except Exception as e:
-                    print(f'Ignoring exception in longpoll cycle:\n{e}', file=sys.stderr)
-                    ts = await self.get_longpoll_server()
+                    ts = await self.get_user_longpoll()
+        raise LoginError('Group token passed to user client')
 
     def run(self, token, owner_id=None):
         self.token = token
