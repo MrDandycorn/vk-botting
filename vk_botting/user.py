7bc9559ed876dd72066b699280c55f8a86d30050
MrDandycorn <ypsen@yandex.ru>
2020-01-11 01:33:56 +0300

    Добавил дополнительные поля для пользователей

--- vk_botting/user.py
+++ vk_botting/user.py
@@ -35,8 +35,17 @@ async def get_own_page(token):
     return User(user.get('response')[0])
 
 
-async def get_users(token, *uids):
-    users = await vk_request('users.get', token, user_ids=','.join(map(str, uids)))
+async def get_users(token, *uids, fields=None, name_case=None):
+    if fields is None:
+        fields = ['photo_id', ' verified', ' sex', ' bdate', ' city', ' country', ' home_town', ' has_photo', ' photo_50', ' photo_100', ' photo_200_orig', ' photo_200',
+                  ' photo_400_orig', ' photo_max', ' photo_max_orig', ' online', ' domain', ' has_mobile', ' contacts', ' site', ' education', ' universities', ' schools',
+                  ' status', ' last_seen', ' followers_count', ' common_count', ' occupation', ' nickname', ' relatives', ' relation', ' personal', ' connections', ' exports',
+                  ' activities', ' interests', ' music', ' movies', ' tv', ' books', ' games', ' about', ' quotes', ' can_post', ' can_see_all_posts', ' can_see_audio',
+                  ' can_write_private_message', ' can_send_friend_request', ' is_favorite', ' is_hidden_from_feed', ' timezone', ' screen_name', ' maiden_name', ' crop_photo',
+                  ' is_friend', ' friend_status', ' career', ' military', ' blacklisted', ' blacklisted_by_me', ' can_be_invited_group']
+    if name_case is None:
+        name_case = 'nom'
+    users = await vk_request('users.get', token, user_ids=','.join(map(str, uids)), fields=fields, name_case=name_case)
     users = users.get('response')
     return [User(user) for user in users]
 
@@ -97,6 +106,67 @@ class User(Messageable):
         self.last_name = data.get('last_name')
         self.is_closed = data.get('is_closed')
         self.can_access_closed = data.get('can_access_closed')
+        self.photo_id = data.get('photo_id')
+        self.verified = data.get('verified')
+        self.sex = data.get('sex')
+        self.bdate = data.get('bdate')
+        self.city = data.get('city')
+        self.country = data.get('country')
+        self.home_town = data.get('home_town')
+        self.has_photo = data.get('has_photo')
+        self.photo_50 = data.get('photo_50')
+        self.photo_100 = data.get('photo_100')
+        self.photo_200_orig = data.get('photo_200_orig')
+        self.photo_200 = data.get('photo_200')
+        self.photo_400_orig = data.get('photo_400_orig')
+        self.photo_max = data.get('photo_max')
+        self.photo_max_orig = data.get('photo_max_orig')
+        self.online = data.get('online')
+        self.domain = data.get('domain')
+        self.has_mobile = data.get('has_mobile')
+        self.contacts = data.get('contacts')
+        self.site = data.get('site')
+        self.education = data.get('education')
+        self.universities = data.get('universities')
+        self.schools = data.get('schools')
+        self.status = data.get('status')
+        self.last_seen = data.get('last_seen')
+        self.followers_count = data.get('followers_count')
+        self.common_count = data.get('common_count')
+        self.occupation = data.get('occupation')
+        self.nickname = data.get('nickname')
+        self.relatives = data.get('relatives')
+        self.relation = data.get('relation')
+        self.personal = data.get('personal')
+        self.connections = data.get('connections')
+        self.exports = data.get('exports')
+        self.activities = data.get('activities')
+        self.interests = data.get('interests')
+        self.music = data.get('music')
+        self.movies = data.get('movies')
+        self.tv = data.get('tv')
+        self.books = data.get('books')
+        self.games = data.get('games')
+        self.about = data.get('about')
+        self.quotes = data.get('quotes')
+        self.can_post = data.get('can_post')
+        self.can_see_all_posts = data.get('can_see_all_posts')
+        self.can_see_audio = data.get('can_see_audio')
+        self.can_write_private_message = data.get('can_write_private_message')
+        self.can_send_friend_request = data.get('can_send_friend_request')
+        self.is_favorite = data.get('is_favorite')
+        self.is_hidden_from_feed = data.get('is_hidden_from_feed')
+        self.timezone = data.get('timezone')
+        self.screen_name = data.get('screen_name')
+        self.maiden_name = data.get('maiden_name')
+        self.crop_photo = data.get('crop_photo')
+        self.is_friend = data.get('is_friend')
+        self.friend_status = data.get('friend_status')
+        self.career = data.get('career')
+        self.military = data.get('military')
+        self.blacklisted = data.get('blacklisted')
+        self.blacklisted_by_me = data.get('blacklisted_by_me')
+        self.can_be_invited_group = data.get('can_be_invited_group')
 
 
 class BlockedUser:
