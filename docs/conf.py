905ac920f2c2cf7910d48af012a5a1d43235e2bb
MrDandycorn <ypsen@yandex.ru>
2020-01-18 02:46:34 +0300

    Добавил API Reference

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@ import sys
 import os
 import re
 import sphinx_rtd_theme
+import sphinxcontrib_trio
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
