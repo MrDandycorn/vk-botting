6c27cf613442ab4c6a506c5568ea5f592a8b7d1b
MrDandycorn <ypsen@yandex.ru>
2020-01-29 22:23:29 +0300

    Убрал зависимость от requests в auth

--- vk_botting/__init__.py
+++ vk_botting/__init__.py
@@ -13,7 +13,7 @@ __title__ = 'vk_botting'
 __author__ = 'MrDandycorn'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2019 MrDandycorn'
-__version__ = '0.6.3'
+__version__ = '0.6.4'
 
 from collections import namedtuple
 import logging
@@ -30,7 +30,7 @@ from vk_botting.auth import TokenReceiverKate, TokenReceiverOfficial
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
 
-version_info = VersionInfo(major=0, minor=6, micro=3, releaselevel='development', serial=0)
+version_info = VersionInfo(major=0, minor=6, micro=4, releaselevel='development', serial=0)
 
 try:
     from logging import NullHandler
