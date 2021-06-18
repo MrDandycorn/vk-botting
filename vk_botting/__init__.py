09a98a247ecaa4078fab0635dbe1b674ab539486
sovamorco <sovamorco@sovamor.co>
2021-06-18 14:01:43 +0300

    поднял версию

--- vk_botting/__init__.py
+++ vk_botting/__init__.py
@@ -13,7 +13,7 @@ __title__ = 'vk_botting'
 __author__ = 'MrDandycorn'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2019-2021 MrDandycorn'
-__version__ = '0.11.0'
+__version__ = '0.11.1'
 
 from collections import namedtuple
 import logging
@@ -29,7 +29,7 @@ from vk_botting.exceptions import *
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
 
-version_info = VersionInfo(major=0, minor=11, micro=0, releaselevel='development', serial=0)
+version_info = VersionInfo(major=0, minor=11, micro=1, releaselevel='development', serial=0)
 
 try:
     from logging import NullHandler
